d7e534fc8366f22a288695de88e088f3a00de3e6
Martin-aaa <ruichem2@uci.edu>
2026-02-25 10:33:22 -0800

    2.25 hand&gun

--- final/Assets/Script/UI.cs
+++ final/Assets/Script/UI.cs
@@ -6,13 +6,7 @@ using UnityEngine;
 public class UI : MonoBehaviour
 {
    
-   [SerializeField] private TMP_Text _testUI;
-    [SerializeField] private Weapon _weapon;
-    
-     // Update is called once per frame
-     void Update()
-     {
-          _testUI.text = _weapon._targetName;
-     }
+
+   
     
 }
--- final/Assets/Script/Weapon.cs
+++ final/Assets/Script/Weapon.cs
@@ -7,30 +7,60 @@ public class Weapon : MonoBehaviour
 {
     private Vector3 _shootRaycastOrigin;
     public string _targetName;
+    [SerializeField] private Animator _HandAnimator;
+    [SerializeField] private Transform _shootpoint;
+    [SerializeField] private float _fireRate = 0.5f;
+    [SerializeField] private float _range = 1000f;
+    [SerializeField] private float _SpreadFactor = 0.1f;
+    [SerializeField] private AudioSource _ShootAudio;
+    [SerializeField] private float _currentBullets;
+    [SerializeField] private float _maxBullets = 30f;
+
+    private float _timer;
+
+
+    private void Start()
+    {
+        _currentBullets = _maxBullets;
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        _timer += Time.deltaTime;
+        if (Input.GetKey(KeyCode.Mouse0) && _timer >= _fireRate)
         {
-           FireWeapon(); 
+            FireWeapon();
+            //_HandAnimator.SetBool("Isshooting",true);
+            _timer = 0;
         }
     }
 
     private void FireWeapon()
     {
-        _shootRaycastOrigin = transform.position;
-        Ray shootRay = new Ray(_shootRaycastOrigin, transform.forward);
-        Physics.Raycast(shootRay, out RaycastHit hitInfo);
-            if(hitInfo.collider != null)
-            {
-                Debug.Log("Hit: " + hitInfo.collider.name);
-                _targetName = hitInfo.collider.name.ToString();
-            }
-            else
-            {
-                Debug.Log("Missed");
-                _targetName = "None";
-            }
+        if (_currentBullets <= 0)
+        {
+            Debug.Log("Out of bullets!");
+            return;
+        }
+        _shootRaycastOrigin = _shootpoint.position;
+        Vector3 shootDirection = _shootpoint.forward;
+        RaycastHit hit;
+        shootDirection = shootDirection + _shootpoint.TransformDirection(new Vector3(Random.Range(-_SpreadFactor, _SpreadFactor), Random.Range(-_SpreadFactor, _SpreadFactor)));
+
+        if (Physics.Raycast(_shootRaycastOrigin, shootDirection, out hit, _range))
+        {
+            Debug.Log("Hit: " + hit.transform.gameObject.name);
+
+        }
+        _currentBullets--;
+        _ShootAudio.Play();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(_shootRaycastOrigin, _shootpoint.forward);
     }
 }
