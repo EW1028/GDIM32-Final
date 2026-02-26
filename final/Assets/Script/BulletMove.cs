a4926976c646f1639edff8c00f1b1b15ab2bf21a
Martin-aaa <ruichem2@uci.edu>
2026-02-25 20:26:03 -0800

    2.25

--- final/Assets/Script/BulletMove.cs
+++ final/Assets/Script/BulletMove.cs
@@ -9,7 +9,7 @@ public class BulletMove : MonoBehaviour
     [SerializeField] private float speed = 1f;
     [SerializeField] private Transform _bulletTransform;
     [SerializeField] private Collider _bulletCollider;
-
+   
 
 
     void Start()
@@ -23,10 +23,7 @@ public class BulletMove : MonoBehaviour
         Vector3 moveDirection = -_bulletTransform.forward.normalized;
         _bulletTransform.position += moveDirection * speed * Time.deltaTime;
     }
-    void OnTriggerEnter(Collider other)
-    {
-        
-    }
+  
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
