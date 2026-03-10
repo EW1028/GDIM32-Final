95fe7d82ee3970d901f88c55d9677ba622c5d570
Martin-aaa <ruichem2@uci.edu>
2026-03-10 13:45:37 -0700

    3.10

---
+++ final/Assets/Script/DialogueScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "DialogueLine", menuName = "ScriptableObjects/DialogueLine", order = 1)]
+public class DialogueScript : ScriptableObject
+{
+    public string[] _lines;
+    public string[] _playerReplyOptions;
+    public DialogueScript[] _npcReplies;
+}
