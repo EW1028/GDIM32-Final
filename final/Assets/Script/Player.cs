faf60c8fc76102504a0dad0314769b61c8d3d3d0
Martin-aaa <ruichem2@uci.edu>
2026-03-14 13:57:14 -0700

    3.14 jump

--- final/Assets/Script/Player.cs
+++ final/Assets/Script/Player.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+
 //using System.Numerics;
 using UnityEngine;
 
@@ -11,7 +13,12 @@ public class Player : MonoBehaviour
     [SerializeField] private float _Speed = 5.0f;
     [SerializeField] private Vector3 __playerVelocity;
     [SerializeField] private GameObject _EnemyPrefab;
-
+    [SerializeField] private float _jump = 10;
+    [SerializeField] private Collider _playercollider;
+    private bool _isGround;
+    Vector3 Dir;
+    Vector3 Velocity;
+    private bool _isjumping;
 
 
     void Start()
@@ -20,22 +27,62 @@ public class Player : MonoBehaviour
     }
     
     private void Update()
-    {
-        
+    {      
         playermove();
-       //enemycreate();
+        jump();
+    }
+    private void FixedUpdate()
+    {
+        if (_isjumping)
+        {
+            _isjumping = false;
+            Velocity.y = _jump;
+            //Debug.Log("jump");
+        }
+        _playerRigidbody.velocity = Velocity;
+        
     }
     private void playermove()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        //float LeftIput = -Input.GetAxis("Horizontal");
-        //float RightIput = Input.GetAxis("Horizontal");
-        Vector3 Dir = (_playerTransform.forward * verticalInput  + _playerTransform.right * horizontalInput).normalized;
-        __playerVelocity = Dir * _Speed;
-        _playerRigidbody.velocity = __playerVelocity;
-        //Debug.Log("verticalInput: " + verticalInput);
-        //Debug.Log("horizontalInput: " + horizontalInput);
+        Dir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Dir += _playerTransform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Dir += -_playerTransform.right;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Dir += _playerTransform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Dir += -_playerTransform.forward;
+        }
+
+        Velocity = Dir.normalized * _Speed;
+        Velocity.y = _playerRigidbody.velocity.y;
+    }
+
+    private void  jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)&& _isGround == true)
+        {
+            _isjumping = true;
+            _isGround = false;
+        }
+    }
+   
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "ground")
+        {
+            _isGround = true;
+        }
     }
 
     private void enemycreate()
